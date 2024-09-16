de1cb1fcfc02554d0fd5cdb2e70e7094d2c520c5
yongs21 <yongs21@douzone.com>
2024-09-16 15:03:11 +0900

    git init

---
+++ java-adv1/src/main/java/mark/multithreadingconcurrency/javaadv1/JavaAdv1Application.java
@@ -0,0 +1,13 @@
+package mark.multithreadingconcurrency.javaadv1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaAdv1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaAdv1Application.class, args);
+	}
+
+}
